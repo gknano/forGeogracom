91f906dde011f4a1fb62a50ad7a089a5c75f5fb1
“greykor” <gkdps@gmail.com>
2021-02-04 14:30:00 +0300

    deleted console.log

--- src/store/index.js
+++ src/store/index.js
@@ -3,5 +3,4 @@ import userInfoReducer from './Reducers';
 
 const initialState = { userInfoReducer: { userInfo: [] } };
 const store = createStore(combineReducers({ userInfoReducer }), initialState);
-console.log(store.getState());
 export default store;
